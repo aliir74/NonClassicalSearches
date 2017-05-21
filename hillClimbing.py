b8d4f0ab7a0027ef8497f3ff0cb47e6fb1b491bc
Ali Irani <aliirani74@gmail.com>
2017-05-21 20:58:31 +0430

    random-restart hill climbing added

--- hillClimbing.py
+++ hillClimbing.py
@@ -43,3 +43,20 @@ def firstChoiceHillClimbing(problem):
                 break
         if(nextState == state):
             return state
+
+def randomRestartHillClimbing(problem):
+    state = problem.initialState()
+    while True:
+        actions = problem.actions(state)
+        nextState = state
+        for idx,a in enumerate(actions):
+            neighbour = problem.result(state, a)
+            if(problem.fitness(neighbour) > problem.fitness(nextState)):
+                nextState = neighbour
+
+        if(state == nextState):
+            if(problem.goalTest(state) == True):
+                return state
+            else:
+                nextState = problem.randomStart()
+        state = nextState
